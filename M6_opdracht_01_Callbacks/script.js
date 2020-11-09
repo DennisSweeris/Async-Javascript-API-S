462279804d08215f09c1f665c596ea7dda94cf22
Dennis Sweeris <dennissweeris@Hotmail.com>
2020-11-09 14:17:42 +0100

    Added opdracht_01-Callbacks

---
+++ M6_opdracht_01_Callbacks/script.js
@@ -0,0 +1,13 @@
+huiswerkMaken = (vak, callback) => {
+    setTimeout(() => {
+        console.log(`Ok, ok, ik ga nu mijn ${vak} huiswerk maken`);
+        callback()
+    }, 1000);
+
+}
+
+klaarMetHuiswerk = () => {
+    console.log("Kijk Paps/Mams, ik ben klaar met mijn huiswerk!")
+};
+
+huiswerkMaken('wiskunde', klaarMetHuiswerk);
